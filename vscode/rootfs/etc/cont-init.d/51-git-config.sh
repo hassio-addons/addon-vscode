1cce6659323625256ac01f11b165ced027bd7100
Paul Sinclair <sinclairpaul@hotmail.com>
2019-03-25 18:56:49 -0400

    :wrench: Move global gitconfig to persistent

---
+++ vscode/rootfs/etc/cont-init.d/51-git-config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Visual Studio Code
+# Sets up the users git global config to be persistent
+# ==============================================================================
+readonly GIT_USER_PATH=/data/git
+
+if ! bashio::fs.directory_exists "${GIT_USER_PATH}"; then
+    mkdir -p "${GIT_USER_PATH}" \
+        || bashio::exit.nok 'Failed to create a persistent git folder'
+
+    chmod 700 "${GIT_USER_PATH}" \
+        || bashio::exit.nok \
+            'Failed setting permissions on persistent git folder'
+fi
+
+ln -s "${GIT_USER_PATH}/.gitconfig" ~/.gitconfig
