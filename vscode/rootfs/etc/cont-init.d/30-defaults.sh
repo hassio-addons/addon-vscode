02b8296a9904c97bebb9f055dba4e6c6eba3376d
Franck Nijhof <frenck@addons.community>
2019-03-28 22:07:51 +0100

    :ambulance: Workaround workspaces.json warnings

--- vscode/rootfs/etc/cont-init.d/30-defaults.sh
+++ vscode/rootfs/etc/cont-init.d/30-defaults.sh
@@ -18,13 +18,20 @@ if ! bashio::fs.file_exists '/data/vscode/User/settings.json'; then
     mkdir -p /data/vscode/User \
         || bashio::exit.nok "Could not create persistent storage folder."
 
-    cp -R /root/.code-server/settings.json /data/vscode/User/settings.json
+    cp /root/.code-server/settings.json /data/vscode/User/settings.json
 fi
 
 # Workaround workspace bug for code-server
 # https://github.com/codercom/code-server/issues/121
-if ! bashio::fs.file_exists '/data/vscode/Backups/workspace.json'; then
+if ! bashio::fs.file_exists '/data/vscode/Backups/workspaces.json'; then
     mkdir -p /data/vscode/Backups \
         || bashio::exit.nok "Could not create persistent storage folder."
-    touch /data/vscode/Backups/workspace.json
+    cp /root/.code-server/workspaces.json /data/vscode/Backups/workspaces.json
+fi
+
+# Workaround workspace bug for code-server (same as above, part 2)
+# https://github.com/codercom/code-server/issues/121
+if ! bashio::fs.file_exists '/data/vscode/User/workspaceStorage'; then
+     mkdir -p /data/vscode/User/workspaceStorage \
+         || bashio::exit.nok "Could not create persistent storage folder."
 fi
