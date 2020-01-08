e87cc08f8f5d149367927dfc21307c60a0348163
MrWHO <mrwho@cerberus.torchetti.us>
2020-01-08 16:07:22 -0600

    Add `allow` configuration for nginx ingress

--- vscode/rootfs/etc/cont-init.d/nginx.sh
+++ vscode/rootfs/etc/cont-init.d/nginx.sh
@@ -4,6 +4,7 @@
 # Configures NGINX for use with code-server
 # ==============================================================================
 declare port
+declare allow
 declare certfile
 declare hassio_dns
 declare ingress_interface
@@ -29,8 +30,10 @@ fi
 
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
+allow=$(bashio::config 'allow')
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%allow%%/${allow}/g" /etc/nginx/servers/ingress.conf
 
 hassio_dns=$(bashio::dns.host)
 sed -i "s/%%hassio_dns%%/${hassio_dns}/g" /etc/nginx/includes/resolver.conf
