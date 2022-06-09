d6d67c975d89907e1385a37a5b76a726bf344995
Franck Nijhof <git@frenck.dev>
2022-06-09 12:10:14 +0200

    ⬆️ Upgrades add-on base image to 6.0.0

--- vscode/rootfs/etc/cont-init.d/mysql.sh
+++ vscode/rootfs/etc/cont-init.d/mysql.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Studio Code Server
 # Pre-configures the MySQL clients, if the service is available
