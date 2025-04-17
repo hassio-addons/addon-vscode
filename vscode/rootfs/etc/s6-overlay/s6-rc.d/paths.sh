72a4bae0853eb30fef71c6446a64e8599df71b02
Richard Sperry <Richard@SperryFamily.net>
2025-04-16 23:17:53 -0700

    devenv

--- vscode/rootfs/etc/s6-overlay/s6-rc.d/paths.sh
+++ vscode/rootfs/etc/s6-overlay/s6-rc.d/paths.sh
@@ -12,3 +12,4 @@ readonly CODE_SERVER_WORKSPACE_FILE="$CODE_SERVER_WORKSPACE_DIR/SERVER.code-work
 readonly CODE_SERVER_VSCODE_DIR="$CODE_SERVER_WORKSPACE_DIR/.vscode"
 readonly CODE_SERVER_VSCODE_TASKS="$CODE_SERVER_VSCODE_DIR/tasks.json"
 readonly -a DIRECTORIES=(addons addon_configs backup config media share ssl)
+readonly CUSTOM_BASH_SCRIPTS_PATH="/data/vscode/custom"
